bfa77f9974c479ef0bfbb881726086aec536ad92
hanandadras <hanan.aabid@gmail.com>
2022-03-29 23:21:05 -0700

    added submit form funtionality

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -11,12 +11,16 @@ function ContactForm() {
     setFormState({...formState, [e.target.name]: e.target.value })
   }
   
-  console.log(formState);
+  // console.log(formState);
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+  }
   return (
     <section>
       <h1>Contact me</h1>
-      <form id="contact-form">
+      <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
           <input type="text" defaultValue={name} onChange={handleChange} name="name" />
